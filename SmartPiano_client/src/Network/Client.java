9bb7f97f85a2f89ed91928508dea94c6f25c92f7
MariaMartori <maria.martorii@students.salle.url.edu>
2020-03-09 21:54:33 +0100

    feat: Complete register implementation.
    Merged development_register into master.

--- SmartPiano_client/src/Network/Client.java
+++ SmartPiano_client/src/Network/Client.java
@@ -71,6 +71,7 @@ public class Client extends Thread {
                         }
                         break;
 
+
                     case "REGISTER":
                         switch (action){
                             case "registered":
